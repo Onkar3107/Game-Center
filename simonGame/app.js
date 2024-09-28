4706d42f20bd1c83ebd5fe1d763ebeb7a50369f6
Onkar3107 <164166762+Onkar3107@users.noreply.github.com>
2024-09-28 23:08:04 +0530

    simon game committed

---
+++ simonGame/app.js
@@ -0,0 +1,98 @@
+let gameSeq = [];
+let userSeq = [];
+
+let colorsInx = ["yellow", "blue", "green", "red"];
+
+let level = 0;
+let started = false;
+let highScore = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+  if (!started) {
+    console.log("Game Started");
+    started = true;
+
+    levelUp();
+  }
+});
+
+function gameFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(() => {
+    btn.classList.remove("flash");
+  }, 300);
+}
+
+function userFlash(btn) {
+  btn.classList.add("userFlash");
+  setTimeout(() => {
+    btn.classList.remove("userFlash");
+  }, 300);
+}
+
+function levelUp() {
+  // console.log("User Seq :- ", userSeq);
+  userSeq = [];
+  level++;
+  h2.innerText = `Level ${level}`;
+
+  // random button choose
+  let randomNum = Math.floor(Math.random() * 4);
+  let randomColor = colorsInx[randomNum];
+  gameSeq.push(randomColor);
+  // console.log("Game Seq :- ", gameSeq);
+  // console.log("*-*-*-");
+  let randomBtn = document.querySelector(`.${randomColor}`);
+  gameFlash(randomBtn);
+}
+
+function checkAns(lastInx) {
+  if (gameSeq[lastInx] === userSeq[lastInx]) {
+    if (gameSeq.length === userSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    console.log("Game Over");
+    h2.innerHTML = `Game Over! Your Score was <b><i>${level}</i></b> <br> Press any key to restart`;
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(() => {
+      document.querySelector("body").style.backgroundColor = "white";
+    }, 150);
+    updateHighScore(level);
+    reset();
+  }
+}
+
+function btnPress() {
+  let btn = this;
+  userFlash(btn);
+
+  let userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+
+  checkAns(userSeq.length - 1);
+}
+
+let btns = document.querySelectorAll(".box");
+
+for (let btn of btns) {
+  btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
+}
+
+function updateHighScore(score) {
+  let h3 = document.querySelector("h3");
+
+  if (score > highScore) {
+    highScore = score;
+    h3.innerHTML = `High Score: <b><i>${highScore}</i></b>`;
+  }
+}
